051cbe0aea4c9750762396d0f4f3d74cfcee6bf7
ktsyr1 <46694587+ktsyr1@users.noreply.github.com>
2022-03-29 11:53:14 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,16 +1,37 @@
+/*
+    1- theme
+    2- button onclick of fatch data
+        - dom load
+        - fatch data for api
+        - display data
+        - alert dane
+        - Waiting 2s 
+        - finished and rest button
+**/
+function ui(json) {
+    // display data
+    document.querySelector('#sell').textContent = json.sell
+    document.querySelector('#buy').textContent = json.buy
+
+    // ui button
+    document.querySelector('.srf button').textContent = 'تم التحديث'
+
+    /*
+        Waiting for a Timeout
+        syntx setTimeout(handler, timeout)
+    */
+    setTimeout(() => {
+        document.querySelector('.srf button').textContent = ' تحديث'
+    }, 2000)
+}
 
 async function srf() {
-    document.querySelector('.srf button').textContent =  'جاري التحديث'
+    document.querySelector('.srf button').textContent = 'جاري التحديث'
 
+    // fetch data 
     let res = await fetch('https://srflb.vercel.app/api/lb')
     let json = await res.json()
-    console.log(json[0]);
-    document.querySelector('#sell').textContent = json[0].sell
-    document.querySelector('#buy').textContent = json[0].buy
-    document.querySelector('.srf button').textContent = 'تم التحديث'
-     
-    setTimeout(() => {
-        document.querySelector('.srf button').textContent =  ' تحديث'
-    
-},2000)
+
+    // ui button and display data
+    return ui(json[0])
 } 
